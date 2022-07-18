0c17928cb7e73a51a953380a354d834b2f9723c9
wwotex <wwojtek55@gmail.com>
2022-07-19 00:39:22 +0200

    bootstrap added and static base template and css

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/', views.UserMainView.as_view()),
-    path('user/<str:pk>', views.LanguageWordsView.as_view()),
+    path('user/', views.UserMainView.as_view(), name='language.list'),
+    path('user/<str:pk>', views.LanguageWordsView.as_view(), name='word.list'),
 ]
