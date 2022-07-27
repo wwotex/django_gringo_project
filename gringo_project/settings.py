fd01cb4e4379f4295c8395583fb531e388840e8f
wwotex <wwojtek55@gmail.com>
2022-07-27 22:03:51 +0200

    doesnt matter

--- gringo_project/settings.py
+++ gringo_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@g_yzp909wn6#u2n9dc06_l*=vd3t*f@5h_32ik*qz_ad#&d22'
+SECRET_KEY = 'that is a secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
